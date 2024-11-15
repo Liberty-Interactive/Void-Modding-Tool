208ed00da906d2c859e225bfdaba0a05c5b46bad
ThePixelMoon <yourlocalmoon7@gmail.com>
2024-11-15 15:51:58 +0200

    add gl

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 // main.cpp
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <gl/gl.h>
+#include <iostream>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
@@ -13,6 +15,54 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         default:
             return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
+}
+
+bool InitializeOpenGL(HWND hwnd) {
+    HDC hdc = GetDC(hwnd);
+    if (!hdc) {
+        return false;
+    }
+
+    PIXELFORMATDESCRIPTOR pfd = {};
+    pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER | PFD_DRAW_TO_WINDOW;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cRedBits = 8;
+    pfd.cGreenBits = 8;
+    pfd.cBlueBits = 8;
+    pfd.cAlphaBits = 8;
+
+    int pixelFormat = ChoosePixelFormat(hdc, &pfd);
+    if (!pixelFormat || !SetPixelFormat(hdc, pixelFormat, &pfd)) {
+        return false;
+    }
+
+    HGLRC hglrc = wglCreateContext(hdc);
+    if (!hglrc || !wglMakeCurrent(hdc, hglrc)) {
+        return false;
+    }
+
+    return true;
+}
+
+void Render(HWND hwnd) {
+    HDC hdc = GetDC(hwnd);
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+#if 0
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f( 0.5f, -0.5f);
+    glVertex2f( 0.0f,  0.5f);
+    glEnd();
+#endif
+
+    SwapBuffers(hdc);
 }
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -30,11 +80,15 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
         CLASS_NAME,
         "Void Modding Tool",
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
         nullptr, nullptr, hInstance, nullptr
     );
 
     if (hwnd == nullptr) {
+        return 0;
+    }
+
+    if (!InitializeOpenGL(hwnd)) {
         return 0;
     }
 
@@ -44,6 +98,8 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     while (GetMessage(&msg, nullptr, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
+
+        Render(hwnd);
     }
 
     return 0;
